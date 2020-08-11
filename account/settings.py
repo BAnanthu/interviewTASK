c89db771666b217dbb80766f3af2e4bbe2441832
NANDHU <ananthu3454@gmail.com>
2020-08-11 21:55:32 +0530

    commit 2

--- account/settings.py
+++ account/settings.py
@@ -129,7 +129,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_HOST_USER ='ananthu3454@gmail.com'
-EMAIL_HOST_PASSWORD = '73566230881'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxx'
 EMAIL_USE_TLS = True
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
